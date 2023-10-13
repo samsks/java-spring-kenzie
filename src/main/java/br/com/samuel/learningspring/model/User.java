9aadcd4c99b080107bbd2f6da64d75119ce814fc
samsks <samuel.meireles.15s@outlook.com>
2023-10-13 10:54:47 -0300

    fix: balance default on User model

--- src/main/java/br/com/samuel/learningspring/model/User.java
+++ src/main/java/br/com/samuel/learningspring/model/User.java
@@ -1,6 +1,7 @@
 package br.com.samuel.learningspring.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Table(name = "users")
@@ -24,7 +25,8 @@ public class User {
     @Column(length = 6, nullable = false)
     private String type;
 
-    @Column(nullable = false, columnDefinition = "DECIMAL DEFAULT 0")
+    @Column(nullable = false, columnDefinition = "DECIMAL")
+    @ColumnDefault("0")
     private float balance;
 
     public User() {
